be330f8674aeb332fb2c2503cbb08bdd373a7459
Naz <bluspringindohq@gmail.com>
2023-09-11 12:46:01 +0800

    Move chickens to be under the same mobcap as zombies

--- src/main/java/xyz/bluspring/crimeutils/mixin/EntityTypeMixin.java
+++ src/main/java/xyz/bluspring/crimeutils/mixin/EntityTypeMixin.java
@@ -3,6 +3,7 @@ package xyz.bluspring.crimeutils.mixin;
 import net.minecraft.core.Registry;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.animal.Chicken;
 import net.minecraft.world.entity.monster.Zombie;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -23,6 +24,17 @@ public class EntityTypeMixin {
                             )
                                     .sized(0.6F, 1.95F)
                                     .clientTrackingRange(8)
+                                    .build(string)
+                    ));
+        } else if (string.equals("chicken")) {
+            cir.setReturnValue(
+                    (EntityType<T>) Registry.register(Registry.ENTITY_TYPE, string,
+                            EntityType.Builder.of(
+                                            (type, level) -> new Chicken((EntityType) type, level),
+                                            CrimeUtilsConfig.getZombieCategory()
+                                    )
+                                    .sized(0.4F, 0.7F)
+                                    .clientTrackingRange(10)
                                     .build(string)
                     ));
         }
